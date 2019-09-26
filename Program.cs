5dff6b175c9fb6107e65f7a6c5ece8cf050165fa
Koshara <ukracent2009@gmail.com>
2019-09-26 06:12:54 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] month = new int[12];
+            for (int i = 0; i < month.Length; i++)
+            {
+                month[i] = DateTime.DaysInMonth(DateTime.Now.Year, i + 1);
+                Console.WriteLine(month[i]);
+            }
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
